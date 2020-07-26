3a829d6200cc79ad497806745635669ea070ddd0
Ronic <abdoronic@gmail.com>
2020-07-26 07:03:43 +0200

    add drawing background

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,18 +1,21 @@
-const WEBSITE_TITLE = 'Website Boilerplate'
-const REPO_NAME = 'website-boilerplate'
-const DOCS_HOMEPAGE_ID = 'doc1'
+const constants = {
+  WEBSITE_TITLE: 'Website Boilerplate',
+  WEBSITE_TAGLINE: 'This is the awesome tagline',
+  REPO_NAME: 'website-boilerplate',
+  DOCS_HOMEPAGE_ID: 'doc1'
+}
 
 module.exports = {
-  title: WEBSITE_TITLE,
-  tagline: `The tagline of ${WEBSITE_TITLE}`,
+  title: constants.WEBSITE_TITLE,
+  tagline: constants.WEBSITE_TAGLINE,
   url: 'https://icpc-guc-community.github.io',
-  baseUrl: `/${REPO_NAME}/`,
+  baseUrl: `/${constants.REPO_NAME}/`,
   favicon: 'img/favicon.ico',
   organizationName: 'icpc-guc-community',
-  projectName: REPO_NAME,
+  projectName: constants.REPO_NAME,
   themeConfig: {
     navbar: {
-      title: WEBSITE_TITLE,
+      title: constants.WEBSITE_TITLE,
       links: [
         {
           to: 'docs/',
@@ -22,7 +25,7 @@ module.exports = {
         },
         { to: 'blog', label: 'Blog', position: 'left' },
         {
-          href: `https://github.com/icpc-guc-community/${REPO_NAME}`,
+          href: `https://github.com/icpc-guc-community/${constants.REPO_NAME}`,
           label: 'GitHub',
           position: 'right'
         }
@@ -30,7 +33,7 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-      copyright: `Made with ❤️ by ICPC GUC Commnuity. Built with Docusaurus.`
+      copyright: `Made with ❤️ by ICPC GUC Commnuity`
     }
   },
   presets: [
@@ -39,13 +42,13 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: DOCS_HOMEPAGE_ID,
+          homePageId: constants.DOCS_HOMEPAGE_ID,
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: `https://github.com/icpc-guc-community/${REPO_NAME}/edit/master/`
+          editUrl: `https://github.com/icpc-guc-community/${constants.REPO_NAME}/edit/master/`
         },
         blog: {
           showReadingTime: true,
-          editUrl: `https://github.com/icpc-guc-community/${REPO_NAME}/edit/master/`
+          editUrl: `https://github.com/icpc-guc-community/${constants.REPO_NAME}/edit/master/`
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,113 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import clsx from 'clsx'
 import Layout from '@theme/Layout'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import useBaseUrl from '@docusaurus/useBaseUrl'
 import styles from './styles.module.css'
+import CanvasDraw from 'react-canvas-draw'
+import { Button } from '@material-ui/core'
+import { CirclePicker } from 'react-color'
+
+const classes = {
+  content: {
+    position: 'absolute',
+    top: '25%',
+    left: 0,
+    right: 0,
+    margin: 'auto',
+    color: 'black'
+  },
+  board: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100vw',
+    height: '100vh'
+  },
+  footer: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    zIndex: 100
+  },
+  footerContainer: {
+    width: '100vw',
+    padding: 16,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+}
 
 function Home() {
   const context = useDocusaurusContext()
   const { siteConfig = {} } = context
+
+  const [brushColor, setBrushColor] = useState('#444')
+  const [contentZIndex, setContentZIndex] = useState(100)
+  const [hideContent, setHideContent] = useState(false)
+
+  const canvasRef = useRef(null)
+
+  const handleDrawing = () => setContentZIndex(1)
+  const handleColorChange = (color) => setBrushColor(color.hex)
+  const handleClear = () => {
+    canvasRef.current.clear()
+    setHideContent(true)
+  }
+
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description='Description will go into a meta tag in <head />'
-    >
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className='container'>
-          <h1 className='hero__title'>{siteConfig.title}</h1>
-          <p className='hero__subtitle'>{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted
-              )}
-              to={useBaseUrl('docs/')}
+        {!hideContent && (
+          <div style={{ ...classes.content, zIndex: contentZIndex }}>
+            <h1 className='hero__title'>{siteConfig.title}</h1>
+            <p className='hero__subtitle'>{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx(
+                  'button button--outline button--secondary button--lg',
+                  styles.getStarted
+                )}
+                to={useBaseUrl('docs/')}
+              >
+                Get Started
+              </Link>
+            </div>
+          </div>
+        )}
+        <div style={classes.board}>
+          <CanvasDraw
+            ref={canvasRef}
+            brushColor={brushColor}
+            canvasWidth='100vw'
+            canvasHeight='100vh'
+            onChange={() => handleDrawing()}
+          />
+        </div>
+        <div style={classes.footer}>
+          <div style={classes.footerContainer}>
+            <Button
+              variant='outlined'
+              style={classes.footerItem}
+              onClick={handleClear}
             >
-              Get Started
-            </Link>
+              Clear
+            </Button>
+            <CirclePicker
+              color={brushColor}
+              onChangeComplete={handleColorChange}
+              colors={[
+                '#444444',
+                '#e91e63',
+                '#ffeb3b',
+                '#673ab7',
+                '#3f51b5',
+                '#4caf50'
+              ]}
+            />
           </div>
         </div>
       </header>
