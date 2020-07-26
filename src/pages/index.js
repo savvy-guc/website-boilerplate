caaa5c9f2313bdf71017a944a0ce735f2e7c53b9
Ronic <abdoronic@gmail.com>
2020-07-26 16:31:05 +0200

    code refactore & removed board for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import styles from './styles.module.css'
 import CanvasDraw from 'react-canvas-draw'
 import { Button } from '@material-ui/core'
 import { CirclePicker } from 'react-color'
+import { useMediaQuery } from 'react-responsive'
 
 const classes = {
   content: {
@@ -16,6 +17,9 @@ const classes = {
     left: 0,
     right: 0,
     margin: 'auto',
+    color: 'black'
+  },
+  button: {
     color: 'black'
   },
   board: {
@@ -41,75 +45,113 @@ const classes = {
   }
 }
 
+function Board({ onDraw, onClear }) {
+  const [brushColor, setBrushColor] = useState('#444')
+  const handleColorChange = (color) => setBrushColor(color.hex)
+
+  const canvasRef = useRef(null)
+
+  const handleDrawing = () => onDraw()
+  const handleClear = () => {
+    canvasRef.current.clear()
+    onClear()
+  }
+
+  return (
+    <React.Fragment>
+      <div style={classes.board}>
+        <CanvasDraw
+          ref={canvasRef}
+          brushColor={brushColor}
+          canvasWidth='100vw'
+          canvasHeight='100vh'
+          onChange={handleDrawing}
+        />
+      </div>
+      <div style={classes.footer}>
+        <div style={classes.footerContainer}>
+          <Button
+            variant='outlined'
+            style={classes.footerItem}
+            onClick={handleClear}
+          >
+            Clear
+          </Button>
+          <CirclePicker
+            color={brushColor}
+            onChangeComplete={handleColorChange}
+            colors={[
+              '#444444',
+              '#e91e63',
+              '#ffeb3b',
+              '#673ab7',
+              '#3f51b5',
+              '#4caf50'
+            ]}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+function HeroContent({ siteConfig }) {
+  return (
+    <div className='container'>
+      <h1 className='hero__title'>{siteConfig.title}</h1>
+      <p className='hero__subtitle'>{siteConfig.tagline}</p>
+      <div className={styles.buttons}>
+        <Link
+          className={clsx(
+            'button button--outline button--secondary button--lg',
+            styles.getStarted
+          )}
+          to={useBaseUrl('docs/')}
+        >
+          <div style={classes.button}>Get Started</div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+function BoardHero({ children }) {
+  const [contentZIndex, setContentZIndex] = useState(100)
+  const [hideContent, setHideContent] = useState(false)
+
+  const onDraw = () => setContentZIndex(1)
+  const onClear = () => setHideContent(true)
+
+  return (
+    <React.Fragment>
+      {!hideContent && (
+        <div style={{ ...classes.content, zIndex: contentZIndex }}>
+          {children}
+        </div>
+      )}
+      <Board onDraw={onDraw} onClear={onClear} />
+    </React.Fragment>
+  )
+}
+
+function ColorHero({ children }) {
+  return <React.Fragment>{children}</React.Fragment>
+}
+
 function Home() {
   const context = useDocusaurusContext()
   const { siteConfig = {} } = context
 
-  const [brushColor, setBrushColor] = useState('#444')
-  const [contentZIndex, setContentZIndex] = useState(100)
-  const [hideContent, setHideContent] = useState(false)
+  const notMobile = useMediaQuery({ query: '(min-width: 481px)' })
 
-  const canvasRef = useRef(null)
-
-  const handleDrawing = () => setContentZIndex(1)
-  const handleColorChange = (color) => setBrushColor(color.hex)
-  const handleClear = () => {
-    canvasRef.current.clear()
-    setHideContent(true)
-  }
+  const Hero = notMobile ? BoardHero : ColorHero
 
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        {!hideContent && (
-          <div style={{ ...classes.content, zIndex: contentZIndex }}>
-            <h1 className='hero__title'>{siteConfig.title}</h1>
-            <p className='hero__subtitle'>{siteConfig.tagline}</p>
-            <div className={styles.buttons}>
-              <Link
-                className={clsx(
-                  'button button--outline button--secondary button--lg',
-                  styles.getStarted
-                )}
-                to={useBaseUrl('docs/')}
-              >
-                Get Started
-              </Link>
-            </div>
-          </div>
-        )}
-        <div style={classes.board}>
-          <CanvasDraw
-            ref={canvasRef}
-            brushColor={brushColor}
-            canvasWidth='100vw'
-            canvasHeight='100vh'
-            onChange={() => handleDrawing()}
-          />
-        </div>
-        <div style={classes.footer}>
-          <div style={classes.footerContainer}>
-            <Button
-              variant='outlined'
-              style={classes.footerItem}
-              onClick={handleClear}
-            >
-              Clear
-            </Button>
-            <CirclePicker
-              color={brushColor}
-              onChangeComplete={handleColorChange}
-              colors={[
-                '#444444',
-                '#e91e63',
-                '#ffeb3b',
-                '#673ab7',
-                '#3f51b5',
-                '#4caf50'
-              ]}
-            />
-          </div>
-        </div>
+        <Hero>
+          <HeroContent siteConfig={siteConfig} />
+        </Hero>
       </header>
     </Layout>
   )
